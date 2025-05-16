ad8f6da78ba1ff37adbee6990e91e00313e16436
eyllcintiriz <127094591+eyllcintiriz@users.noreply.github.com>
2025-05-16 15:02:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,43 @@
 //js 1111
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
   //userName ekledik. by createUsernames()
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
+
 //jd 2222
 const account2 = {
   owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -21,6 +47,7 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+  currency: 'GBP',
 };
 
 const account4 = {
@@ -28,6 +55,7 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+  currency: 'EUR',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -64,95 +92,18 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-//Fonksionlar, eventListenerlar
-//LOG IN
-let currentAccount;
-btnLogin.addEventListener('click', function (e) {
-  e.preventDefault();
-
-  currentAccount = accounts.find(
-    acc => acc.userName === inputLoginUsername.value
-  );
-
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    // Başarılı giriş
-    labelWelcome.textContent = `Welcome back, ${
-      currentAccount.owner.split(' ')[0]
-    }!`;
-    // input'ları temizle
-    inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur(); // focus'u kaldırır
-    document.addEventListener('DOMContentLoaded', function () {
-      displayMovements(currentAccount.movements);
-    });
-
-    containerApp.style.opacity = 1;
-    updateUI(currentAccount);
-  } else {
-    // Giriş başarısızsa gizli kalsın
-    containerApp.style.opacity = 0;
-    labelWelcome.textContent = `Hatalı Giriş`;
-  }
-});
+//FUNCTIONS
 const updateUI = function (account) {
   displayMovements(account.movements);
   printCalcBalance(currentAccount);
   printOutSummary(currentAccount);
   printInSummary(currentAccount);
 };
-
-//TRANSFER
-btnTransfer.addEventListener('click', function (e) {
-  e.preventDefault();
-  let amount = Number(inputTransferAmount.value);
-  let person = accounts.find(acc => acc.userName === inputTransferTo.value);
-
-  if (
-    amount > 0 &&
-    person &&
-    currentAccount.balance >= amount &&
-    person.userName !== currentAccount.userName
-  ) {
-    currentAccount.movements.push(-amount); // gönderen
-    person.movements.push(amount); // alıcı
-  }
-  updateUI(currentAccount);
-  inputTransferAmount.value = inputTransferTo.value = 0;
-});
-
-//REQUEST LOAN????
-btnLoan.addEventListener('click', function (e) {
-  e.preventDefault();
-  let amount = Number(inputLoanAmount.value);
-  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    currentAccount.movements.push(amount);
-    updateUI(currentAccount);
-    inputLoanAmount.value = ' ';
-  }
-});
-
-//CLOSE ACCOUNT
-btnClose.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log(accounts);
-  if (
-    inputCloseUsername.value === currentAccount.userName &&
-    Number(inputClosePin.value) === currentAccount.pin
-  ) {
-    const index = accounts.findIndex(
-      acc => acc.userName === currentAccount.userName
-    );
-    accounts.splice(index, 1);
-    containerApp.style.opacity = 0;
-    labelWelcome.textContent = `The account has been successfully closed`;
-  }
-  console.log(accounts);
-});
-
 //movement kutusu
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = ' ';
-  movements.forEach(function (movedMoney, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (movedMoney, i) {
     const type = movedMoney > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -167,11 +118,6 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-document.addEventListener('DOMContentLoaded', function () {
-  displayMovements(currentAccount.movements);
-});
-//
-
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
     acc.userName = acc.owner
@@ -213,6 +159,94 @@ const printOutSummary = function (user) {
   labelSumInterest.textContent = `${interest} €`;
 };
 
+//EVENTLISTENERS
+//LOG IN
+let currentAccount;
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.userName === inputLoginUsername.value
+  );
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Başarılı giriş
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }!`;
+    // input'ları temizle
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur(); // focus'u kaldırır
+    document.addEventListener('DOMContentLoaded', function () {
+      displayMovements(currentAccount.movements);
+    });
+
+    containerApp.style.opacity = 1;
+    updateUI(currentAccount);
+  } else {
+    // Giriş başarısızsa gizli kalsın
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = `Hatalı Giriş`;
+  }
+});
+
+//TRANSFER
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  let amount = Number(inputTransferAmount.value);
+  let person = accounts.find(acc => acc.userName === inputTransferTo.value);
+
+  if (
+    amount > 0 &&
+    person &&
+    currentAccount.balance >= amount &&
+    person.userName !== currentAccount.userName
+  ) {
+    currentAccount.movements.push(-amount); // gönderen
+    person.movements.push(amount); // alıcı
+  }
+  updateUI(currentAccount);
+  inputTransferAmount.value = inputTransferTo.value = 0;
+});
+
+//REQUEST LOAN
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  let amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+    inputLoanAmount.value = ' ';
+  }
+});
+
+//CLOSE ACCOUNT
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(accounts);
+  if (
+    inputCloseUsername.value === currentAccount.userName &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.userName === currentAccount.userName
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = `The account has been successfully closed`;
+  }
+  console.log(accounts);
+});
+//SORT
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
+
+//TIMER
+
 //DEĞER HESAPLAMA
 //giriş
 let deposit = currentAccount.movements
@@ -230,10 +264,29 @@ const max = currentAccount.movements.reduce((acc, cur) => {
   else return cur;
 });
 
-/*
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-*/
+const exchangeRates = {
+  EUR: {
+    USD: 1.08,
+    GBP: 0.85,
+    EUR: 1,
+  },
+  USD: {
+    EUR: 0.93,
+    GBP: 0.79,
+    USD: 1,
+  },
+  GBP: {
+    EUR: 1.18,
+    USD: 1.27,
+    GBP: 1,
+  },
+};
+
+const groupedByActivity = Object.groupBy(accounts, account => {
+  const movementCount = account.movements.length;
+
+  if (movementCount >= 8) return 'very active';
+  else if (movementCount >= 4) return ' active';
+  else if (movementCount >= 1) return 'moderate';
+  return 'inactive';
+});
